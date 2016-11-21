641e0cb1fa9972820758e032f7205ece0d63410e
Allen Short <ashort@mozilla.com>
2016-11-21 09:34:44 -0600

    schema for sqlalchemy, basic test support

--- redash/cli/database.py
+++ redash/cli/database.py
@@ -2,13 +2,15 @@ from flask_script import Manager
 
 manager = Manager(help="Manage the database (create/drop tables).")
 
+
 @manager.command
 def create_tables():
     """Create the database tables."""
-    from redash.models import create_db, init_db
-
-    create_db(True, False)
+    from redash.models import db, create_db, init_db
+    create_db(True, True)
     init_db()
+    db.session.commit()
+
 
 @manager.command
 def drop_tables():
